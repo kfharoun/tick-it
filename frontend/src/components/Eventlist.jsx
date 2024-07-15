eb9495d51499567b89ae6b3535953794ec690cd5
AzaleaTopaz <azaleahowland@gmail.com>
2024-07-15 12:02:21 -0400

    put in all export default function boilerplate

---
+++ frontend/src/components/Eventlist.jsx
@@ -0,0 +1,6 @@
+export default function Eventlist() {
+    return 
+    <div>
+        <h1>Event List</h1>
+    </div>
+}
---
+++ frontend/src/components/Venuepage.jsx
@@ -0,0 +1,9 @@
+import Eventlist from './Eventlist'
+
+
+export default function Venuepage() {
+    return 
+    <div>
+        <h1>Venue Page</h1>
+    </div>
+}
