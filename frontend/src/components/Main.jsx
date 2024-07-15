a04d914ad80bee1b20baa5cd47897ce0cceed5f6
MyCloudAtl <megandmashburn@gmail.com>
2024-07-15 15:40:10 -0400

    azalea

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
-import Bandpage from './Bandpage';
+import Artistpage from './Artistpage';
 import Eventpage from './Eventpage';
 import Venuepage from './Venuepage';
 import Eventlist from './Eventlist';
