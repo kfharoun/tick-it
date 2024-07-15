0f5483cf68dfd9c248d88e331f160a1da6d8c55c
MyCloudAtl <megandmashburn@gmail.com>
2024-07-15 14:28:12 -0400

    proper syntax

--- frontend/src/components/Bandpage.jsx
+++ frontend/src/components/Bandpage.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
 export default function Bandpage () {
-    return 
+    return (
     <div classname = 'Bandpage'>
        <h1>Band Page</h1>
     </div>
-}
+    )
+ }
