c8bd756da931da18d357ca62ae3c787fdc8692af
MyCloudAtl <megandmashburn@gmail.com>
2024-07-16 13:34:45 -0400

    Eventspage rendering data

--- frontend/src/components/Eventpage.jsx
+++ frontend/src/components/Eventpage.jsx
@@ -1,9 +1,45 @@
-import Venuepage from './Venuepage'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios' 
 
-export default function Eventpage(){
+export default function Event() {
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        const eventData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/events/')
+                if (response.status !== 200) {
+                    throw new Error('Not working')
+                }
+                setEvents(response.data)
+            } catch (error) {
+                console.error('Error grabbing events:', error)
+            }
+        };
+    eventData ();
+    }, []);
+
     return (
-    <div>
-        <h1>Event Page</h1>
+    <div className = 'Event'>
+       <h1>Event page</h1>
+       <ul>
+       {events.map(event => (
+           <li key={event.id}>
+           <img src={event.image_url} alt={event.name} />
+           <div>
+             <h2>Name: {event.name}</h2>
+             <h3>Address: {event.address}</h3>
+             <h3>Date: {event.date}</h3>
+             <h3>Email: {event.contact_email}</h3>
+             <h3>Phone: {event.contact_phone}</h3>
+             <h3>Accessible Seating: {event.accessible_seating}</h3>
+             <h3>Capacity: {event.capacity}</h3>
+             <h4>Parking: {event.parking}</h4>
+             <p>Parking Specifics: {event.parking_specifics}</p>
+           </div>
+         </li>
+       ))}
+     </ul>
     </div>
-    )
+    );
 }
