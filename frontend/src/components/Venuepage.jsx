b44c1af0e2d4a77974e736d7ac81d73b3dd1635c
AzaleaTopaz <azaleahowland@gmail.com>
2024-07-17 11:08:09 -0400

    got the upcoming events rendered for the specific venue on the page

--- frontend/src/components/Venuepage.jsx
+++ frontend/src/components/Venuepage.jsx
@@ -6,26 +6,30 @@ import { useParams, Link } from 'react-router-dom';
 
 export default function Venuepage() {
   const [venue, setVenue] = useState(null);
-  const [artist, setArtist] = useState(null);
+  const [events, setEvents] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const { id } = useParams();
 
   useEffect(() => {
-    const getVenue = async () => {
+    const getVenueAndEvents = async () => {
       try {
         const venueResponse = await axios.get(`http://localhost:8000/venues/${id}`);
         const venueData = venueResponse.data;
         setVenue(venueData);
         console.log(venueData);
 
-        const artistResponse = await axios.get(`http://localhost:8000/artists/${id}`);
-        const artistData = artistResponse.data;
-        setArtist(artistData);
-        console.log(artistData);
-
+        const eventsResponse = await axios.get(`http://localhost:8000/events/`);
+        const eventsData = eventsResponse.data;
+        console.log(eventsData)
         
+
+       const venueEvents = eventsData.filter(event => event.venues.some(venue => venue.id === parseInt(id)))
+        setEvents(venueEvents);
+        console.log(venueEvents)
+
+
       } catch (err) {
         console.error('Error fetching venue', err.message);
         setError('There was a problem fetching venue data. Please try again.');
@@ -34,7 +38,7 @@ export default function Venuepage() {
       }
     };
 
-    getVenue();
+    getVenueAndEvents();
   }, [id]);
 
 
@@ -42,6 +46,8 @@ export default function Venuepage() {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
+    
+console.log(events)
   return (
     <div className='Venuepage'>
       <h1>Venue: {venue.name}</h1>
@@ -56,17 +62,30 @@ export default function Venuepage() {
           <h2>Accessible seating? {venue.accessible_seating ? '✅' : '❌'}</h2>
           <h2>Is there parking? {venue.parking ? '✅' : '❌'}</h2>
           <a className="footerlink" href={`mailto:${venue.contact_email}`}>email</a>
-
-
+    </div> 
+    <div className = 'upcoming-events'>
+          {/* <Eventlist venueId={venue.id} /> */}
+          <h2>Upcoming Events</h2>
+          {events.length > 0 ? (
+            events.map(event => (
+              <div key={event.id} className='event'>
+                <p>{new Date(event.date).toLocaleDateString()}</p>
+                <h3>{event.name}</h3>
+                {event.artists && event.artists.map(artist => (
+                    <div key={artist.id} className='artist'>
+                      <img src={artist.image_url} alt={artist.name} />
+                      <p>{artist.name}</p>
+                    </div>
+                  ))}
+              </div>
+            ))
+            ) : (
+              <p>No events found</p>
+            )}
           </div>
-          <div className = 'upcoming-events'>
-          <Eventlist venueId={venue.id} />
-
-          <img src={artist.image_url} alt={artist.name} />
          
-          
-
-          </div>
+           
+        
         </div>
       ) : (
         <p>No venue found</p>
@@ -74,3 +93,10 @@ export default function Venuepage() {
     </div>
   );
 }
+          
+          
+          
+         
+       
+        
+
